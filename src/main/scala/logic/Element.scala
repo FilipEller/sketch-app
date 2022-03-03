e876781c4865060a8b8f0d2b41b3ceb90a8416c3
Filip <filip.eller@aalto.fi>
2022-03-03 15:00:54 +0200

    Add Element class.

---
+++ src/main/scala/logic/Element.scala
@@ -0,0 +1,15 @@
+package logic
+
+import scalafx.scene.paint.Color
+import scalafx.scene.canvas.Canvas
+
+abstract case class Element(id: String, origin: Point, rotation: Int, hidden: Boolean,
+                            deleted: Boolean, group: Option[Long], color: Color, previousVersion: Option[Element]) {
+
+  def paint(canvas: Canvas): Unit
+
+  def move(newOrigin: Point): Element
+
+  def rotate(angle: Int): Element
+
+}
