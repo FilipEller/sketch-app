cd4e8479c301b25ed26d078993415199203d0195
Filip <filip.eller@aalto.fi>
2022-03-03 19:27:03 +0200

    Add Element class.

--- src/main/scala/logic/Element.scala
+++ src/main/scala/logic/Element.scala
@@ -4,7 +4,7 @@ import scalafx.scene.paint.Color
 import scalafx.scene.canvas.Canvas
 
 abstract case class Element(id: String, origin: Point, rotation: Int, hidden: Boolean,
-                            deleted: Boolean, group: Option[Long], color: Color, previousVersion: Option[Element]) {
+deleted: Boolean, group: Option[Long], color: Color, previousVersion: Option[Element]) {
 
   def paint(canvas: Canvas): Unit
 
