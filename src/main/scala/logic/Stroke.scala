8c78ef9f3238fab78bd746f7f2eda885c239af72
Filip <filip.eller@aalto.fi>
2022-03-18 16:02:20 +0200

    Adjust brush opacity.

--- src/main/scala/logic/Stroke.scala
+++ src/main/scala/logic/Stroke.scala
@@ -14,22 +14,20 @@ case class Stroke(id: String, color: Color, origin: Point2D, path: Path, brush: 
 
   def paint(canvas: Canvas): Unit = { // there's a problem with drawing less opaque brush strokes.
     val g = canvas.graphicsContext2D  // Brush images are painted too closely for the opacity to have much effect, but painting them further apart would leave each brush image distinctly visible.
-    g.setGlobalAlpha(this.color.getOpacity * 0.25) // setting the opacity this way doesn't really help
-    val red = math.round(color.getRed * 255).toInt
-    val green = math.round(color.getGreen * 255).toInt
-    val blue = math.round(color.getBlue * 255).toInt
-    val rgbColor = rgb(red, green, blue)
-    val gradient = new RadialGradient(
+    val opacity = math.pow(this.color.opacity, 1.5) / (this.brush.hardness / 100.0 * 0.3 * this.brush.size) // this is fixing opacity with brush size 30 but other sizes have to be tested. With this 5 % opacity is actually invisible though.
+    val usedColor = new Color(this.color.opacity(opacity))
+    println(usedColor.opacity)
+    val gradient = new RadialGradient( // this should take brush hardness into account somewhere.
        0, 0,  // focus angle, focus distance
        0.5, 0.5,  // center x, y
        0.5, // radius
        true,  // proportional
        CycleMethod.NoCycle,
-       List(Stop(0.0, rgbColor), Stop(1.0, Color.Transparent))
+       List(Stop(0.0, usedColor), Stop(1.0, Color.Transparent))
     )
     // rotation not implemented
     g.fill = gradient
-    this.path.foreach(point => g.fillOval(point.x, point.y, this.brush.size, this.brush.size))
+    this.path.foreach(point => g.fillOval(point.x - 0.5 * this.brush.size, point.y - 0.5 * this.brush.size, this.brush.size, this.brush.size))
     g.setGlobalAlpha(1)
   }
 
