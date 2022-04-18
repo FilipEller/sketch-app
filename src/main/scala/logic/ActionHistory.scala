1944cfe796b7f607cff0efcbdacee2a21dc97acc
Filip <filip.eller@aalto.fi>
2022-04-18 13:11:32 +0300

    Remove print statements from ActionHistory.scala.

--- src/main/scala/logic/ActionHistory.scala
+++ src/main/scala/logic/ActionHistory.scala
@@ -7,20 +7,16 @@ object ActionHistory {
   val actions: Stack[Seq[Element]] = Stack()
 
   def add(element: Element): Unit = {
-    println("ADDING HISTORY:" + element + ", " + element.previousVersion)
     this.actions.push(Seq(element))
   }
 
   def add(elements: Seq[Element]): Unit = {
-    println("ADDING HISTORY:" + elements + ", " + elements.map(_.previousVersion))
     this.actions.push(elements)
   }
 
   def undo(): Seq[Element] = {
     if (this.actions.nonEmpty) {
-      val result = this.actions.pop()
-      println("undoing: " + result + ", " + result.map(_.previousVersion))
-      result
+      this.actions.pop()
     } else {
       Seq()
     }
