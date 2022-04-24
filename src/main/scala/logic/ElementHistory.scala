d39d6c263dea2dd21c2911ba86e5d685a7e4fd38
Filip <filip.eller@aalto.fi>
2022-04-24 19:20:45 +0300

    Clear the ElementHistory when a Drawing is created or loaded.

--- src/main/scala/logic/ElementHistory.scala
+++ src/main/scala/logic/ElementHistory.scala
@@ -4,22 +4,31 @@ import scala.collection.mutable.Stack
 
 object ElementHistory {
 
-  val actions: Stack[Seq[Element]] = Stack()
+  // Stores the most recently changed Elements in a Stack
+  private val actions: Stack[Seq[Element]] = Stack()
 
+  // Adds a new Element on the top of the Stack
   def add(element: Element): Unit = {
     this.actions.push(Seq(element))
   }
 
+  // Adds a sequence of Elements on the top of the Stack
   def add(elements: Seq[Element]): Unit = {
     this.actions.push(elements)
   }
 
+  // Pops the top of the Stack, if it is not empty
   def undo(): Seq[Element] = {
     if (this.actions.nonEmpty) {
       this.actions.pop()
     } else {
       Seq()
     }
+  }
+
+  // Empties the Stack. This is done when a new Drawing is loaded.
+  def clear(): Unit = {
+    this.actions.clear()
   }
 
 }
